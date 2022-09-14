620f66f3b9d359510f92c4e8a93720ba0b4d727d
Ali Ahmad <aliaurangzaib05@gmail.com>
2022-09-14 11:47:00 +0500

    first commit

---
+++ cypress/e2e/jsonobjects.cy.js
@@ -0,0 +1,41 @@
+/// <reference types="cypress" />
+
+describe('Json Objects',()=>{
+
+    it('Json Objects',()=>{
+        //cy.openHomePage()
+
+        const simpleobject = {"key": "value","key2": "value2","key3": "value3"}
+
+        const simpleArrayofValues = ["one", "two", "three"]
+
+        const ArrayofObjects = [{"key": "value"},{"key2": "value2"},{"key3": "value3"}]
+
+        const typesofData = { "string": "Hi, My name is Ali", "number": 5 }
+
+        const mix = {
+            "First Name": "Ali",
+            "Last Name": "Ahmad",
+            "Age": 25,
+            "Students":[
+                {
+                    "firstName": "Sara",
+                    "lastName": "Jokovic"
+                },
+                {
+                    "firstName": "Shumaa",
+                    "lastName": "Nide"
+                }
+
+            ]
+        }
+
+        console.log(simpleobject.key2)
+        console.log(simpleobject["key2"])
+        console.log(simpleArrayofValues[1])
+        console.log(ArrayofObjects[2].key3)
+        console.log(mix.Students[1].firstName)
+    })
+
+
+})
